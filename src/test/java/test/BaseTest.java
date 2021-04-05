2fe309f360ac7987c5d1df06fdf192ea155345a6
anastasiya.krivokora <anastasiya.krivokora@quadcode.com>
2021-04-05 14:32:08 +0300

    Initial commit

---
+++ src/test/java/test/BaseTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class BaseTest {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized = true;
+        Configuration.baseUrl = "https://github.com";
+    }
+}
